f61c3a7ef22cb9f341e8aaca9e135f7502692dc6
RivaJ <3110103518@zju.edu.cn>
2024-07-20 20:22:39 +0800

    feat: Chapter12

--- code/test.py
+++ code/test.py
@@ -5,9 +5,11 @@ from tools import gcd, phi, factoringPrimeFactors
 
 # print(phi(10000))
 
-for i in range(1000000):
-    if len(factoringPrimeFactors(i)) == 1 and i ** 1000 % 10000 != 1:
-        print(i)
-        break
+# for i in range(1000000):
+#     if len(factoringPrimeFactors(i)) == 1 and i ** 1000 % 10000 != 1:
+#         print(i)
+#         break
 
 # print(443**4 % 10000)
+
+print(factoringPrimeFactors(931))
