e1964a9ff2b537798e7044bf86ce4d28bf42e6c4
RivaJ <3110103518@zju.edu.cn>
2024-07-28 00:11:55 +0800

    feat: Chapter18

--- code/exe17_6.py
+++ code/exe17_6.py
@@ -1,8 +1,8 @@
 from tools import gcd, phi, gcdWithXY, successive_square
 
 
-''' 解同余式x^k≡b(mod m)'''
 def solution(k, b, m):
+    ''' 解同余式x^k≡b(mod m)'''
     if gcd(b, m) != 1:
         raise Exception('b and m must be coprime')
     _phi = phi(m)
@@ -13,5 +13,6 @@ def solution(k, b, m):
         return successive_square(b, u, m)
 
 if __name__ == '__main__':
-    print(solution(113, 347, 463))
-    print(solution(275, 139, 588))
+    print(solution(1789, 5192, 7081))
+    print(solution(1789, 2604, 7081))
+    print(solution(1789, 4222, 7081))
--- code/tools/__init__.py
+++ code/tools/__init__.py
@@ -1,12 +1,4 @@
 from .prime import *
 from .congruence import *
+from .rsa import *
 
-''' a^k(mod m) '''
-def successive_square(a, k, m):
-    b = 1
-    while k >= 1:
-        if k % 2 == 1:
-            b = (b * a) % m
-        a = a * a % m
-        k = k // 2
-    return b
